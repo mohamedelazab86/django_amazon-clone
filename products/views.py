5f8761c68a9892693e0f87bebee37d0575287b33
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2023-12-25 19:18:38 +0200

    create productlist and product detail

--- products/views.py
+++ products/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from .models import Product,Brand,Review,ProductImage
+from django.views.generic import ListView,DetailView
 
 # Create your views here.
+
+# create crud opertions
+class ProductList(ListView):
+    model=Product
+
+class ProductDetail(DetailView):
+    model=Product
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["reviews"] =Review.objects.filter(product=self.get_object()) 
+        context["images"] =ProductImage.objects.filter(product=self.get_object())
+        context["products"] =Product.objects.filter(brand=self.get_object().brand)
+        return context
+    
+
+class BrandList(ListView):
+    model=Brand
+
+class BrandDetail(DetailView):
+    model=Brand
